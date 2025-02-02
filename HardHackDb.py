e4c80e391f3a826bf4821dda994704f5f7b361bd
AlexWoods1 <160354642+AlexWoods1@users.noreply.github.com>
2025-02-02 07:58:24 -0800

    Add files via upload

--- HardHackDb.py
+++ HardHackDb.py
@@ -45,7 +45,48 @@ print(df['Sunlight'].value_counts())
 df['Watering'] = df['Watering'].apply(regularize_watering)
 df["Sunlight"] = df["Sunlight"].apply(regularize_sun)
 print(df)
-df.iloc[:10].to_csv("small_cleaned_plants.csv", index=False)
+#df.to_csv("small_cleaned_plants.csv", index=False)
 
-print(df.set_index(['Sunlight', 'Watering']))
-print(df.to_xarray())
+print(df[(df['Sunlight'] == 1) & (df['Watering'] == 'moist')]['Plant Name'].to_list())
+print(df[(df['Sunlight'] == 1) & (df['Watering'] == 'regular')]['Plant Name'].to_list())
+print(df[(df['Sunlight'] == 1) & (df['Watering'] == 'dry')]['Plant Name'].to_list())
+
+print(df[(df['Sunlight'] == 0.5) & (df['Watering'] == 'moist')]['Plant Name'].to_list())
+print(df[(df['Sunlight'] == 0.5) & (df['Watering'] == 'regular')]['Plant Name'].to_list())
+print(df[(df['Sunlight'] == 0.5) & (df['Watering'] == 'dry')]['Plant Name'].to_list())
+
+print(df[(df['Sunlight'] == 0) & (df['Watering'] == 'moist')]['Plant Name'].to_list())
+print(df[(df['Sunlight'] == 0) & (df['Watering'] == 'regular')]['Plant Name'].to_list())
+print(df[(df['Sunlight'] == 0) & (df['Watering'] == 'dry')]['Plant Name'].to_list())
+
+
+def User_number(x, y):
+    if x == 1:
+        if y == 'moist':
+            return 1
+        elif y == 'regular':
+            return 2
+        else:
+            return 3
+    elif x == 0.5:
+        if y == 'moist':
+            return 4
+        elif y == 'regular':
+            return 5
+        else:
+            return 6
+    elif x == 0:
+        if y == 'moist':
+            return 7
+        elif y == 'regular':
+            return 8
+        else:
+            return 9
+
+
+df = df.assign(control_num=df.get(["Sunlight", "Watering"]).apply(lambda row: User_number(row['Sunlight'], row['Watering']), axis=1))
+
+df_simple = df.get(['Plant Name', 'control_num'])
+df_simple = df_simple.assign(number=1)
+df_simple = df_simple.groupby(['control_num', "Plant Name"]).nunique().reset_index().set_index('control_num').drop('number', axis=1)
+df_simple.to_csv("Numeric_df.csv")
